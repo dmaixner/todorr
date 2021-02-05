686e87999c9a0c4bdb424d183f7882bfe1e40dc1
David Maixner <dmaixner@gmail.com>
2021-02-05 14:17:07 +0100

    [enh] todos saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,2 +1,27 @@
+import { all, call, takeLatest, put } from 'redux-saga/effects';
+import { setTodos, FETCH_TODOS } from '../actions';
+
+const fetchTodosFromApi = () => {
+    return fetch("http://localhost:8080/todos")
+        .then(res => res.json())
+        .then(json => json)
+        .catch(err => { });
+}
+
+function* fetchTodos() {
+    const tempData = yield call(fetchTodosFromApi);
+    yield put(setTodos(tempData));
+}
+
+function* watchFetchTodos() {
+    yield takeLatest(FETCH_TODOS, fetchTodos);
+}
+
+const todosSagas = [
+    fetchTodos(),
+    watchFetchTodos()
+];
+
 export default function* rootSaga() {
+    yield all(todosSagas);
 }
