c7d627d82499aa325d6275f5b95031e0c902d693
David Maixner <dmaixner@gmail.com>
2021-02-06 11:39:41 +0100

    [ref] panel colors and responsivness

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <section className="section">
       <div className="columns is-centered">
-        <div className="column is-6 is-5-fullhd">
+        <div className="column is-12-mobile is-10-tablet is-8-desktop is-7-widescreen is-6-fullhd">
           <TodoListContainer />
         </div>
       </div>
